50aaa9f91a9c8516c9b4e32be6913777a177fe6b
gahyeon <pgh2031@inpix.com>
2023-08-09 14:00:12 +0900

    #3.3 optional props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Circle from './Circle';
 function App() {
   return (
     <div>
-      <Circle bgColor="slateblue" />
+      <Circle bgColor="slateblue" borderColor="darkblue" text="동그라미" />
       <Circle bgColor="salmon" />
     </div>
   )
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,24 +2,40 @@ import styled from 'styled-components';
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 // styled component에 props type 전달
 const Container = styled.div<ContainerProps>`
-  width: 20rem;
-  height: 20rem;
-  border-radius: 10rem;
+  width: 10rem;
+  height: 10rem;
+  border-radius: 5rem;
   background-color: ${props => props.bgColor};
+  border: 1px solid ${props => props.borderColor};
+  text-align: center;
+  color: white;
 `;
 
+// ※ props가 필수가 아닌 선택사항일 경우, props명 뒤에 '?' 표기
 interface CircleProps {
-  bgColor: string;
+  bgColor: string; // is required
+  borderColor?: string; // is optional
+  text?: string;
 }
 
 // 일반 component에 props type 전달
-function Circle({ bgColor } : CircleProps) {
+function Circle({ bgColor, borderColor, text = "default text" } : CircleProps) {
+  // ※ prop type에 디폴트값을 주는 방법: ↑↑↑ 1. 인자값에 디폴트값 지정(function Circle({ ... text = "default text" }))
   return (
-    <Container bgColor={bgColor} />
+    // ※ prop type에 디폴트값을 주는 방법: ↓↓↓ 2. props받을 때 디폴트값 지정 (borderColor={borderColor ?? bgColor)
+
+    // Circle 컴포넌트에서 선택적으로 부여하는 borderColor props는 선택사항이므로 주어지지 않을 수 있지만,
+    // 최종적으로 렌더링되는 Container styled component에서는 border스타일을 그리기 위해 borderColor props가 필수로 type되어 있다. (borderColor: string;)
+    // ∴ Circle 컴포넌트로부터 borderColor props가 주어지지 않았을 경우(=> borderColor: undefined), 
+    // Container 컴포넌트의 borderColor props 디폴트값으로 bgColor props값을 전달한다. (=> borderColor ?? bgColor)
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
   )
 }
 
