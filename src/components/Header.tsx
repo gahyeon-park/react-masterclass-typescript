97a8666f85dc057ca6a70efdc3d773a206d56cda
gahyeon <pgh2031@inpix.com>
2023-08-11 10:14:25 +0900

    #4.4 useNavigate

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-// ※ <Link>를 사용하려면 Header 컴포넌트가 Router 안에 있어야한다. (Router 밖에서는 Link를 렌더할 수 없음)
 function Header() {
+  const navigate = useNavigate();
+  const onAboutClick = () => {
+    navigate("/about");
+  }
   return (
     <header>
       <ul>
@@ -9,7 +12,7 @@ function Header() {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <button onClick={onAboutClick}>About</button>
         </li>
       </ul>
     </header>
