5e75acc23539de938e6c32cf760a9e7ccedcb451
gahyeon <pgh2031@inpix.com>
2023-08-27 14:00:17 +0900

    #5.13 Price cHart part 2

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,28 @@
 const BASED_URL = `https://api.coinpaprika.com/v1`;
 
-export const fetchCoins = async() => {
-  return fetch(`${BASED_URL}/coins`).then((res => res.json()));
-}
+export const fetchCoins = async () => {
+  return fetch(`${BASED_URL}/coins`).then((res) => res.json());
+};
 
-export const fetchCoinInfo = async(coinId: string) => {
+export const fetchCoinInfo = async (coinId: string) => {
   return await (await fetch(`${BASED_URL}/coins/${coinId}`)).json();
-}
+};
 
-export const fetchCoinTickers = async(coinId: string) => {
+export const fetchCoinTickers = async (coinId: string) => {
   return await (await fetch(`${BASED_URL}/tickers/${coinId}`)).json();
-}
+};
 
-export const fetchCoinHistory = async(coinId: string) => {
+export const fetchCoinHistory = async (coinId: string) => {
   // const endDate = Math.floor(Date.now() / 1000); // Date.now(): 1970년 1월 1일 0시 0분 0초부터 현재까지 경과된 밀리초를 반환
   // const startDate = endDate - (60 * 60 * 24 * 7); // startDate = endDate로부터 일주일 전: endDate - (60초 * 60분 * 24시간 * 7일 => 7일(168시간)을 초단위부터 계산)
   // const beforeOneDay = endDate - (60 * 60 * 23);
-  
-  // return await (await fetch(`${BASED_URL}/coins/${coinId}/ohlcv/historical?start=${beforeOneDay}&end=${endDate}`)).json();
-  return await (await fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`)).json();
-}
+
+  // console.log(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`);
+
+  // return await (
+  //   await fetch(`${BASED_URL}/coins/${coinId}/ohlcv/historical`)
+  // ).json();
+  return await (
+    await fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`)
+  ).json();
+};
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,7 @@
 // import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface IChartProps {
   coinId: string;
@@ -17,20 +18,62 @@ interface IOhlcv {
   market_cap: number;
 }
 
-function Chart({ coinId } : IChartProps) {
-  // Chart 스크린에서 코인아이디를 얻는 방법
-  // 1. 라우터로부터 파라미터 가져오기 (useParams)
-  // const params = useParams();
-  // 2. 이미 Coin 스크린에서 Chart 스크린을 렌더하고 있으므로, Coin에서 쓰고있는 coinId를 props로 받아오기
-
+function Chart({ coinId }: IChartProps) {
   // ohlcv =>  Open/High/Low/Close values (시가/고가/저가/종가 값)
-  const { isLoading, data } = useQuery<IOhlcv>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
-  
-    return (
-    <>
-      {isLoading ? "Loading..." : <div>{data?.open}</div>}
-    </>
-  )
+  const { isLoading, data } = useQuery<IOhlcv[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+
+  const getDate = (milliSecond: string) => {
+    const dateIns = new Date(parseInt(milliSecond, 10));
+    const month = dateIns.getMonth() + 1; // 달의 경우 반환값 0부터 시작(1월: 0 ~ 12월: 11)
+    const date = dateIns.getDate();
+
+    return `${month}/${date}`;
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "price",
+              data: data ? data.map((price) => Number(price.close)) : [],
+            },
+          ]}
+          options={{
+            chart: {
+              width: 500,
+              height: 300,
+              background: "transparent",
+              toolbar: {
+                show: false,
+              },
+            },
+            grid: {
+              show: false,
+            },
+            xaxis: {
+              categories: data?.map((price) => getDate(price.time_close)),
+            },
+            yaxis: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+            },
+            theme: {
+              mode: "dark",
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
