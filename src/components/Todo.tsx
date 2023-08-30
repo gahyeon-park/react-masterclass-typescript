d6c2c0411e16fe41ba9069b1f23d02d3f8d2dc9f
gahyeon <pgh2031@inpix.com>
2023-08-30 15:09:03 +0900

    #6.15 change todo category( Immutability part 2)

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -18,16 +18,31 @@ function Todo({ text, category, id }: ITodo) {
   const todos = useRecoilValue(todoListState);
   const setTodos = useSetRecoilState(todoListState);
   
+  console.log(todos);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const { currentTarget: { name }} = event;
-    console.log(todos);
     
     setTodos(oldTodos => {
       const targetIdx = oldTodos.findIndex(todo => todo.id === id);
-      const oldTodo = oldTodos[targetIdx];
-      const newTodo = {text, id, category: name};
+      const newTodo = { text, id, category: name as ITodo["category"] };
+
+      // ※ 기존 todoList 배열 순서를 그대로 유지한 채, 현재 todo의 category만 업데이트한 새 배열이 반환되어야 함.
+      const frontArr = oldTodos.slice(0, targetIdx);
+      const backArr = oldTodos.slice(targetIdx+1, oldTodos.length);
       
-       return oldTodos;
+      // 1.1 category값을 onClick 이벤트 객체에서 받을 경우의 타입 지정
+      // ↓↓↓↓↓↓↓↓↓↓↓↓↓  ★ TS 에러  ↓↓↓↓↓↓↓↓↓↓↓↓↓
+      // Types of property 'category' are incompatible.
+      // Type 'string' is not assignable to type '"TODO" | "DOING" | "DONE"'.
+      // newTodo의 category에 할당한 name의 type이 string 콘크리트 타입으로 되어있다. (↔ ITodo에는 값이 반드시 "TODO"|"DOING"|"DONE" 중 하나여야 한다고 선언되어 있으므로 에러가 발생하는 것)
+      // ∴ 위에 newTodo 초기화에서 category값을 name as any 또는 name as ITodo["category"] 등으로 설정하면 OK.
+      oldTodos = [
+        ...frontArr,
+        newTodo,
+        ...backArr
+      ]
+      
+      return oldTodos;
     });
   }
 
@@ -40,6 +55,8 @@ function Todo({ text, category, id }: ITodo) {
     </Li>
   );
 
+  // 1.2 category값을 onClick 이벤트리스너에 인자로 전달할 경우의 category 타입 지정
+  // 이벤트핸들러에서 파라미터로 받을 때, ITodo["category"] 인터페이스로 타입 지정
   // const onClick = (newCategory: ITodo["category"]) => {
   // }
 
