1f3f8c0291c4ca21520b3e65c1f07432f462b6b2
gahyeon <pgh2031@inpix.com>
2023-08-30 14:25:58 +0900

    #6.13~14 change todo category

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { ITodo } from './atoms';
+import { ITodo, todoListState } from './atoms';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 const Li = styled.li`
   padding-left: 10px;
@@ -13,8 +14,43 @@ const Li = styled.li`
   }
 `;
 
-function Todo({ text }: ITodo) {  // props의 type이 ITodo 인터페이스이기 때문에, ITodo에 정의된 속성(id, text, category) 3개를 전부 props로 받아야 한다.
-  return <Li>{text}</Li>;
+function Todo({ text, category, id }: ITodo) {
+  const todos = useRecoilValue(todoListState);
+  const setTodos = useSetRecoilState(todoListState);
+  
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const { currentTarget: { name }} = event;
+    console.log(todos);
+    
+    setTodos(oldTodos => {
+      const targetIdx = oldTodos.findIndex(todo => todo.id === id);
+      const oldTodo = oldTodos[targetIdx];
+      const newTodo = {text, id, category: name};
+      
+       return oldTodos;
+    });
+  }
+
+  return (
+    <Li>
+      <span>{text}</span>
+      {category !== "TODO" && <button name="TODO" onClick={onClick}>To do</button>}
+      {category !== "DOING" && <button name="DOING" onClick={onClick}>Doing</button>}
+      {category !== "DONE" && <button name="DONE" onClick={onClick}>Done</button>}
+    </Li>
+  );
+
+  // const onClick = (newCategory: ITodo["category"]) => {
+  // }
+
+  // return (
+  //   <Li>
+  //     <span>{text}</span>
+  //     {category !== "TODO" && <button onClick={() => onClick('TODO')}>To do</button>}
+  //     {category !== "DOING" && <button onClick={() => onClick('DOING')}>Doing</button>}
+  //     {category !== "DONE" && <button onClick={() => onClick('DONE')}>Done</button>}
+  //   </Li>
+  // );
 }
 
 export default Todo;
