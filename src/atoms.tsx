140045dcdc9ed7a31fd16cc6ab31af43dd91672c
gahyeon <pgh2031@inpix.com>
2023-09-05 16:59:17 +0900

    #7.14 Creating Tasks (todo 항목을 string에서 객체로 변환)

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -2,15 +2,20 @@
 import { atom, selector } from 'recoil'; 
 // selector: atom을 가져와서 그 atom의 "output"을 변형시켜주는 도구 (★state 자체는 바꾸지 않음★)
 
+export interface ITodo {
+  id: number;
+  text: string;
+}
+
 interface ITodoListState {
-  [key: string]: string[];
+  [key: string]: Array<ITodo>;
 }
 
 export const todoListState = atom<ITodoListState>({
   key: "toDo",
   default: {
-    "to do": ["a", "b"],
-    doing: ["c", "d", "E"],
-    done: ["F"]
+    "to do": [{ id: 0, text: "출근"}],
+    doing: [{ id: 1, text: "근무"}],
+    done: [{id: 2, text: "기상"}]
   }
 });
