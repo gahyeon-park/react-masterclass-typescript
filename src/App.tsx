d3334984f4650ca8a5a01285ff567599a08576db
gahyeon <pgh2031@inpix.com>
2023-09-07 15:18:14 +0900

    #8.3 Variants part 1

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,16 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, borderRadius: "50px", transition: { type: "spring", bounce: 0.3, delay: 0.5 }}
+}
+
 function App() {
  
   return (
     <Wrapper>
-      <Box 
-        transition={{ type: "spring", bounce: 0.3, delay: 0.5 }} 
-        initial={{ scale: 0 }} 
-        animate={{ scale: 1, rotateZ: 360, borderRadius: "50px" }}
-      />
-      {/* <motion.div></motion.div> */}
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   )
 }
