6c64c03f7d28e8aac1d460a98ef7bc6b3b048c93
gahyeon <pgh2031@inpix.com>
2023-09-13 17:37:51 +0900

    #8.12 Slider part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import styled from 'styled-components';
-import { motion, AnimatePresence } from 'framer-motion';
-import { useState } from 'react';
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   display: flex;
+  flex-direction: column;
   width: 100vw;
-  height: 100vh;  
+  height: 100vh;
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
@@ -13,37 +14,55 @@ const Wrapper = styled(motion.div)`
 
 const Box = styled(motion.div)`
   position: absolute;
+  top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100px;
   height: 100px;
+  border-radius: 10px;
   background-color: white;
-  top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1);
+  font-size: 28px;
 `;
 
 const boxVariant = {
-  initial: { opacity: 0, scale: 0 },
-  show: { opacity: 1, scale: 1, rotateZ: 360 },
-  hide: { opacity: 0, scale: 0, y: -100 }
-}
+  inVisible: { x: 500, opacity: 0, scale: 0 },
+  visible: { x: 0, opacity: 1, scale: 1, transition: { duration: 1 } },
+  exit: {
+    x: -500,
+    opacity: 0,
+    scale: 0,
+    rotateX: 180,
+    transition: { duration: 1 },
+  },
+};
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing(prev => !prev);
-  }
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
   return (
     <Wrapper>
-      <button onClick={onClick}>Click</button>
-      <AnimatePresence>{showing ? 
-        <Box 
-          variants={boxVariant} 
-          initial="initial" 
-          animate="show" 
-          exit="hide"  
-        /> : null}
+      <AnimatePresence>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              variants={boxVariant}
+              initial="inVisible"
+              animate="visible"
+              exit="exit"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={prevPlease}>prev</button>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
-  )
+  );
 }
 
 export default App;
